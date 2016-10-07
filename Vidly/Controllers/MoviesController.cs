f3ef2b06490448680f581d920b60b16ab1dc6fcc
MIKIEJ-DESKTOP\mikiej <mikiej_boy@hotmail.com>
2016-10-07 14:46:15 +0100

    Added Overview of possible ActionResults to be returned - added Bootstrap theme - lumen

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -9,7 +9,26 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie { Name = "Shrek" };
-            return View(movie);
+
+            //return View(movie);
+
+            ////[[return just text]] ////
+            //return Content("Hello world");
+
+            ////[[return Page not found]]////
+            //return HttpNotFound("Broken");
+
+            ////[[Empty no results]]////
+            //return new EmptyResult();
+
+            ///[[Redirect to another page]]/////
+            ///param 1 = "method", param 2 = "Controller"  - returns to home page and the url doesnt show
+            //return RedirectToAction("Index", "Home");
+
+            ///[[Redirect to another page]]/////
+            ///param 1 = "method", param 2 = "Controller"  - returns to home page and the url doesnt show 
+            ///however - passing params does shows up in url
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
     }
 }
