1ef497bcf2f82d9d0a91fe93dace5f46d75de277
MIKIEJ-DESKTOP\mikiej <mikiej_boy@hotmail.com>
2016-10-10 21:22:56 +0100

    Added Auto Custom Route, and Sent Model data to view using custom view model

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,5 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -28,7 +30,33 @@ namespace Vidly.Controllers
             ///[[Redirect to another page]]/////
             ///param 1 = "method", param 2 = "Controller"  - returns to home page and the url doesnt show 
             ///however - passing params does shows up in url
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+
+
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name= "Customer 1" },
+                new Customer {Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+
+            return View(viewModel);
+
+        }
+
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        //Localhost:16608/movies/released/2016/04
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
