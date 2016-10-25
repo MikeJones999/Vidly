7d5ba936e26da230dd0fff4f78335ac5fcd37d02
MIKIEJ-DESKTOP\mikiej <mikiej_boy@hotmail.com>
2016-10-25 20:42:37 +0100

    Added data annotations in Customer  to change BirthDate to Date of birth

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -19,6 +19,7 @@ namespace Vidly.Models
         public MembershipType MembershipType { get; set; }
 
         
+        [Display(Name = "Date of Birth")]
         public DateTime? Birthdate { get; set; }
 
         //foreign key
