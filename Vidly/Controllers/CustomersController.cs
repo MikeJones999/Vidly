0fd4fc3a75247313abb23dba6b52aab8088e3a4e
MIKIEJ-DESKTOP\mikiej <mikiej_boy@hotmail.com>
2016-10-26 20:16:55 +0100

    Added Ability to view and amend a customer - update not yet coded. Currently creates a new customer only

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -57,13 +57,13 @@ namespace Vidly.Controllers
         {
             var membershipTypes = _context.MembershipTypes.ToList();
 
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipTypes
               
             };
 
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
         }
 
         [HttpPost]
@@ -72,6 +72,27 @@ namespace Vidly.Controllers
             _context.Customers.Add(Customer);
             _context.SaveChanges();
             return RedirectToAction("Index", "Customers");
+        }
+
+
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
         }
     }
 }
